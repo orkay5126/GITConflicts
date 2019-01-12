8deeb251b03c0a21973f076a9040eec3c2d34bb5
orkay5126 <orkay5126@gmail.com>
2019-01-12 12:21:24 -0500

    Initial Commit

---
+++ src/main/java/CheckConflicts/CheckConflicts/App1.java
@@ -0,0 +1,23 @@
+package CheckConflicts.CheckConflicts;
+
+/**
+ * Hello world!
+ *
+ */
+public class App1 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! - WorkSpace A " );
+    }
+    
+    public static void funWSA()
+    {
+        System.out.println( "Function - WorkSpace A " );
+    }
+    
+    public static void funWSB()
+    {
+        System.out.println( "Function - WorkSpace B " );
+    }
+}
